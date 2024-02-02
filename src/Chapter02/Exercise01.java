7da31bbbaa777053a4c9ed7240cb5c173f5dec7e
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-03 01:08:17 +0300

    initial commit

---
+++ src/Chapter02/Exercise01.java
@@ -0,0 +1,14 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a degree in Celsius: ");
+        double celcius = input.nextDouble();
+        double fahreneit = (9.0/5.0)*celcius+32;
+        System.out.println(celcius+"Celsius is "+fahreneit+"Fahreneit");
+
+    }
+}
