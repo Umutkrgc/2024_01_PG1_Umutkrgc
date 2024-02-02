820fc7eee0edae7e654877ff6ae1baae2471da04
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-03 01:42:22 +0300

    initial commit

---
+++ src/Chapter02/Exercise03.java
@@ -0,0 +1,14 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a value for feet: ");
+        double foot = input.nextDouble();
+        double meters = foot*0.305;
+        System.out.println(foot+" feet is "+meters+" meters");
+
+    }
+}
