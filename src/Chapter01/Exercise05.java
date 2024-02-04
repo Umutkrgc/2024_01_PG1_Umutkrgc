2e03729a2c511ac47ea7f5eac32ae1764825c44a
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-04 21:21:24 +0300

    initial commit

---
+++ src/Chapter01/Exercise05.java
@@ -0,0 +1,6 @@
+public class Exercise05 {
+    public static void main(String[] args) {
+        //Verilen islemin sonucunu veren kod
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
---
+++ src/Chapter01/Exercise10.java
@@ -0,0 +1,12 @@
+public class Exercise10 {
+    public static void main(String[] args) {
+        //Assume a runner runs 14 kilometers in 45 minutes and 30
+        //seconds. Write a program that displays the average speed in miles per hour.
+        //hiz=mesafe/zaman
+        //speed=distance/time
+        double distance = 14 / 1.6;
+        double time = 60 / 45.30;
+        double speed = distance / time;
+        System.out.println("mil/h= " + speed);
+    }
+}
