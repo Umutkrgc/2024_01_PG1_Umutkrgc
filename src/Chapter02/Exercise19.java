783ee75a0b596cc0f4a779ecdf59fddaaa1cf279
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-04 20:04:18 +0300

    initial commit

---
+++ src/Chapter02/Exercise19.java
@@ -0,0 +1,31 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the coordinates of three points (x1, y1), (x2, y2), (x3, y3):");
+        System.out.print("Enter x1 and y1: ");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+
+        System.out.print("Enter x2 and y2: ");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+
+        System.out.print("Enter x3 and y3: ");
+        double x3 = input.nextDouble();
+        double y3 = input.nextDouble();
+
+        // Length of sides
+        double side1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double side2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+        double side3 = Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2));
+
+        double s = (side1 + side2 + side3) / 2;
+        double area = Math.pow((s * (s - side1) * (s - side2) * (s - side3)), 0.5);
+
+        System.out.println("The area of the triangle is " + area);
+    }
+}
