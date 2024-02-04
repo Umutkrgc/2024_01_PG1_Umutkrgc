7f01b9baf0a7c21e85280991aea391be57726576
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-04 17:17:10 +0300

    initial commit

--- src/Chapter02/Exercise08.java
+++ src/Chapter02/Exercise08.java
@@ -1,7 +1,22 @@
 package Chapter02;
 
+import java.util.Scanner;
+
 public class Exercise08 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the time zone offset to GMT: ");
+        int offset = input.nextInt();
 
+        long totalMillisecond = System.currentTimeMillis();
+        long totalSeconds = totalMillisecond / 100;
+        long currentSecond = totalSeconds % 60;
+        long totalMinutes = totalSeconds / 60;
+        long currentMinute = totalMinutes % 60;
+        long totalHours = totalMinutes / 60;
+        long currentHour = (totalHours + offset) % 24;
+
+        System.out.println("Current time in GMT" + offset + " is " + currentHour + ":" + currentMinute + ":" + currentSecond);
+        //dogrulugundan emin degilim.
     }
 }
---
+++ src/Chapter02/Exercise09.java
@@ -0,0 +1,15 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class Exercise09 {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        System.out.println("Enter v0, v1, and t: ");
+        double velocity0 = input.nextDouble();
+        double velocity1 = input.nextDouble();
+        double time = input.nextDouble();
+        double average = (velocity1 - velocity0) / time ;
+        System.out.println("The average acceleration is " + average);
+    }
+}
