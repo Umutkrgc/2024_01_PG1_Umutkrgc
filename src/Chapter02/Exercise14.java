1004eee32e2da86e3d8fc5d0b9438140269b4bae
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-04 18:28:22 +0300

    initial commit

---
+++ src/Chapter02/Exercise14.java
@@ -0,0 +1,17 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter weight in pounds: ");
+        double pounds = input.nextDouble();
+        System.out.println("Enter height in inches: ");
+        double inches = input.nextDouble();
+        double kilograms = pounds * 0.45359237;
+        double meters = inches * 0.0254;
+        double bmi = kilograms / (meters * meters);
+        System.out.println("BMI is " + bmi);
+    }
+}
