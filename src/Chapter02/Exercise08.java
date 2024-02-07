f6e94d785f4fcb37553354eb64ef38af2e2ee7ae
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-07 18:33:53 +0300

    initial commit

--- src/Chapter02/Exercise08.java
+++ src/Chapter02/Exercise08.java
@@ -14,7 +14,7 @@ public class Exercise08 {
         long totalMinutes = totalSeconds / 60;
         long currentMinute = totalMinutes % 60;
         long totalHours = totalMinutes / 60;
-        long currentHour = (totalHours + offset) % 24;
+        long currentHour = totalHours  % 24;
 
         System.out.println("Current time in GMT" + offset + " is " + currentHour + ":" + currentMinute + ":" + currentSecond);
         //dogrulugundan emin degilim.
---
+++ src/Chapter02/Exercise21.java
@@ -0,0 +1,23 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter investment amount: ");
+        double investmentAmount = input.nextDouble();
+
+        System.out.print("Enter annual interest rate: ");
+        double annualInterestRate = input.nextDouble();
+
+        System.out.print("Enter number of years: ");
+        double numberOfYears = input.nextDouble();
+
+        double monthlyInterestRate = annualInterestRate / 100 / 12;
+        double futureInvestmentValue = investmentAmount * (Math.pow((1 + monthlyInterestRate), (numberOfYears * 12)));
+
+        System.out.println("Accumulated vale is $" + futureInvestmentValue);
+    }
+}
