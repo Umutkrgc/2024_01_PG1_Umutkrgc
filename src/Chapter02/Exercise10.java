7d2eb2b1985c873cd920081ec6bd7cc1b5da1ebc
Umutkrgc <umutkaraagac8@gmail.com>
2024-02-07 18:48:19 +0300

    initial commit

--- src/Chapter02/Exercise10.java
+++ src/Chapter02/Exercise10.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Exercise10 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter the amount of water in kilograms: ");
         double waterWeight = input.nextDouble();
+
         System.out.println("Enter the initial temperature: ");
         double initialTemperature = input.nextDouble();
+
         System.out.println("Enter the final temperature: ");
         double finalTemperature = input.nextDouble();
+
         double energy = waterWeight * (finalTemperature - initialTemperature) * 4184;
         System.out.println("The energy needed is " + energy);
     }
