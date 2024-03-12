289c4ef521a0427fd5e23ff8c6ab7945c390c1a6
Umutkrgc <umutkaraagac8@gmail.com>
2024-03-12 16:41:11 +0300

    initial commit

---
+++ src/Chapter03/Exercise16.java
@@ -0,0 +1,19 @@
+package chapter03;
+
+import java.util.Random;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        int centerX = 0;
+        int centerY = 0;
+        int width = 100;
+        int height = 200;
+
+        Random random = new Random();
+        int randomX= centerX-width/2+random.nextInt(width);
+        int randomY= centerY-width/2+random.nextInt(height);
+
+        System.out.println("Random Coordinate in Rectangle:");
+        System.out.println("(" + randomX + ", " + randomY + ")");
+    }
+}
